9ff9890ef76cc847bcd47eb61fb606354f31df49
Simone Nielsen <simonenielsen95@gmail.com>
2026-09-25 12:18:18 +0200

    booking repos (start)

---
+++ DiveDeepEF/Persistence/BasketItemRepository.cs
@@ -0,0 +1,26 @@
+﻿using DiveDeepEF.Models.Bookings;
+
+namespace DiveDeepEF.Persistence
+{
+    public class BasketItemRepository
+    {
+        private static readonly List<BasketItem> items = new();
+
+        public static List<BasketItem> GetAll()
+        {
+            return items;
+        }
+        public static void Add(BasketItem item)
+        {
+            items.Add(item);
+        }
+        public static void Remove(int index)
+        {
+            if (index >= 0 && index < items.Count)
+            {
+                items.RemoveAt(index);
+            }
+        }
+
+    }
+}
---
+++ DiveDeepEF/Persistence/BookingRepository.cs
@@ -0,0 +1,22 @@
+﻿using DiveDeepEF.Models.Bookings;
+using DiveDeepEF.Models.Equipments;
+
+namespace DiveDeepEF.Persistence
+{
+    public class BookingRepository
+    {
+        private static List<Booking> bookings = new List<Booking>();
+
+        public static List<Booking> GetAll()
+        {
+            return bookings;
+        }
+        public static void Add(Booking booking)
+        {
+            if (booking == null) return;
+            booking.Id = bookings.Any() ? bookings.Max(x => x.Id) + 1 : 1;
+            bookings.Add(booking);
+        }
+
+    }
+}

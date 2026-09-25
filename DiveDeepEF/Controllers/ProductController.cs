bfccc70462c383679d1fb69aea2c4b278048c992
Franz Moeglich <franzmoglich@hotmail.com>
2026-09-25 12:17:57 +0200

    tilføjet paths til nogle views, (A) skal skiftes til @ som de bliver brugt

---
+++ DiveDeepEF/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepEF.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Equipment() => View();
+        /* linker ikke lige nu da der ikke er repo, flyt som repo er added
+            public IActionResult Fins() => View(FinsRepository.GetAll());
+            public IActionResult BCD() => View(BcdRepository.GetAll());
+            public IActionResult Mask() => View(MaskRepository.GetAll());
+            public IActionResult Regulator() => View(RegulatorSetRepository.GetAll());
+            public IActionResult Suit() => View(SuitRepository.GetAll());
+            public IActionResult Tank() => View(TankRepository.GetAll());
+        */
+    }
+}
